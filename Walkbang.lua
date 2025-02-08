d889899832ab6223bcd872b5c52b51dc0e53dd51
ches <94960650+FRAGBOMBBLITZ@users.noreply.github.com>
2025-02-08 13:08:21 +1300

    Add files via upload

---
+++ Walkbang.lua
@@ -0,0 +1,15 @@
+--[=[
+
+
+db   d8b   db  .d8b.  db      db   dD d8888b.  .d8b.  d8b   db  d888b  
+88   I8I   88 d8' `8b 88      88 ,8P' 88  `8D d8' `8b 888o  88 88' Y8b 
+88   I8I   88 88ooo88 88      88,8P   88oooY' 88ooo88 88V8o 88 88      
+Y8   I8I   88 88~~~88 88      88`8b   88~~~b. 88~~~88 88 V8o88 88  ooo 
+`8b d8'8b d8' 88   88 88booo. 88 `88. 88   8D 88   88 88  V888 88. ~8~ 
+ `8b8' `8d8'  YP   YP Y88888P YP   YD Y8888P' YP   YP VP   V8P  Y888P  
+                                                    
+To walkbang a player, hover over them and press B to toggle, or use the GUI.
+Script by Ches / @FRAGBOMBBLITZ, have fun!                                                   
+
+]=]
+loadstring(game:HttpGet("https://raw.githubusercontent.com/FRAGBOMBBLITZ/releases/refs/heads/main/Walkbang.lua"))()
